2fb0bad8517df277a81e8ca2295b26d37cd0e0ae
ZheTao <zacharytao99@gmail.com>
2022-07-09 17:14:27 +0800

    swipper feature

---
+++ data/slides.tsx
@@ -0,0 +1,26 @@
+export default [
+{
+    id: '1',
+    title: 'Test1',
+    description: 'this is a new description',
+    image: require('../assets/image1.png'),
+
+},
+
+{
+    id: '2',
+    title: 'Test2',
+    description: 'this is the second description',
+    image: require('../assets/image2.png'),
+
+},
+
+{
+    id: '3',
+    title: 'Test3',
+    description: 'this is a new description',
+    image: require('../assets/image3.png'),
+
+},
+
+]
